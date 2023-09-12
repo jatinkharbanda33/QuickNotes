76c730ede96617b641f635d25c50306008547675
Jatin Kharbanda <76038276+jatinkharbanda33@users.noreply.github.com>
2023-09-12 13:20:21 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
 		<header className='my-4 mx-2'>
 			<div className='navbar bg-primary text-primary-content rounded-lg'>
 				<a className='btn btn-ghost normal-case text-xl' href='/'>
-					NOTEr
+					QuickNotes
 				</a>
 
 				<label
